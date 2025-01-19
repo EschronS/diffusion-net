2f0c09c22f5d6992d1a2ed0edb4056931d36a6c1
EschronS <Erik.Schlenzka@gmx.de>
2025-01-19 16:27:59 +0100

    geometry.py changes to pass trough face tensor when computing pointclouds

--- src/diffusion_net/geometry.py
+++ src/diffusion_net/geometry.py
@@ -422,6 +422,7 @@ def compute_operators(verts, faces, k_eig, normals=None):
 
 def get_all_operators(verts_list, faces_list, k_eig, op_cache_dir=None, normals=None):
     N = len(verts_list)
+    is_cloud = len(faces_list) == 0
 
     frames = [None] * N
     massvec = [None] * N
@@ -444,14 +445,14 @@ def get_all_operators(verts_list, faces_list, k_eig, op_cache_dir=None, normals=
         if normals is None:
             outputs = get_operators(
                 verts_list[i],
-                faces_list[i],
+                faces_list if is_cloud else faces_list[i],
                 k_eig,
                 op_cache_dir,
             )
         else:
             outputs = get_operators(
                 verts_list[i],
-                faces_list[i],
+                faces_list if is_cloud else faces_list[i],
                 k_eig,
                 op_cache_dir,
                 normals=normals[i],
